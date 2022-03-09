4a86f1e587da3f07d3d50603bdb90b4c57422066
Vikas Verma <vikas.verma@helloalfred.com>
2022-03-09 11:34:32 +0530

    Adding docker compose file to run db as service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { CoffeeController } from './coffee/coffee.controller';
 import { CoffeeService } from './coffee/coffee.service';
 import { CoffeeModule } from './coffee/coffee.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeeModule],
+  imports: [CoffeeModule, TypeOrmModule.forRoot({
+    type: 'postgres', // type of our database
+      host: 'localhost', // database host
+      port: 5431, // database host
+      username: 'postgres', // username
+      password: 'root', // user password
+      database: 'postgres', // name of our database,
+      autoLoadEntities: true, // models will be loaded automatically 
+      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
